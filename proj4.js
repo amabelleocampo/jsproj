d42099bfdaee6281c515b2bae400093a76728a47
amaocampo <amabelleocampo@gmail.com>
2016-03-19 16:47:41 -0700

    Create proj4.js

---
+++ proj4.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+	 	<title>JavaScript</title>
+	</head>
+	<body>
+		<script>
+		var a = "amabelle"; 
+		if (a = "Amabelle") {
+			alert(a); 
+		} else {
+			alert("The condition is false");
+		}
+		</script>
+			</body>
+			</html>
