415028718bdc53b00d700ccccade99f4a53569ea
amaocampo <amabelleocampo@gmail.com>
2016-03-19 16:23:43 -0700

    Added files via upload

---
+++ proj3.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+	 	<title>JavaScript</title>
+	
+		<script>
+function sayHello(who) {
+	document.write("Hello," + who); 
+	}
+			</script>
+	</head>
+	<body>
+		<script>
+	sayHello ("Bob");
+	document.write("<br>");
+	sayHello ("Amabelle"); 
+		</script>
+			</body>
+			</html>
