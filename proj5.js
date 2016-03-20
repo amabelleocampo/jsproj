df03c7d0c22dc384758e3a9ddd7ac249321a019c
amaocampo <amabelleocampo@gmail.com>
2016-03-19 17:04:22 -0700

    Added files via upload

---
+++ proj5.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+	 	<title>JavaScript</title>
+	</head>
+	<body>
+		<script>
+		for (i=0; i<10; ++i) {
+			document.write ("How do I iterate?" + i + "<br>"); 
+		}
+		
+		//continue
+		</script>
+			</body>
+			</html>
+			
